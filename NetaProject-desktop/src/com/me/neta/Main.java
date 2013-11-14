68f02c562b89ab59cea856bc7f02b97dea6f2cc5
sashonk <sashonk@9faebf61-7de2-4690-a8d6-888312a489b9>
2013-11-14 11:32:29 +0000

    import

---
+++ NetaProject-desktop/src/com/me/neta/Main.java
@@ -0,0 +1,17 @@
+package com.me.neta;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Was ist das?";
+		cfg.useGL20 = false;
+	//	cfg.fullscreen = true;
+		cfg.width = 1024;
+		cfg.height = 768;
+		
+		new LwjglApplication(new NetaGame(), cfg);
+	}
+}
